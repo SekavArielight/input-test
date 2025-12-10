cd7f9fcd5c946fd6f0461d84aa06cefc747101e6
God'swill Sekav Apya <godswillapya@gmail.com>
2025-12-10 12:53:16 +0100

    Refactor input handling and add name prompt function

--- main.go
+++ main.go
@@ -2,28 +2,30 @@
 // to take input from the user
 package main
 
-import "fmt"
+// import "fmt"
 
 // main function
 func main() {
 
     // Println function is used to
     // display output in the next line
-    fmt.Println("Enter Your First Name: ")
+    // fmt.Println("Enter Your First Name: ")
 
     // var then variable name then variable type
-    var first string
+    // var first string
 
     // Taking input from user
-    fmt.Scanln(&first)
-    fmt.Println("Enter Second Last Name: ")
-    var second string
-    fmt.Scanln(&second)
+    // fmt.Scanln(&first)
+    // fmt.Println("Enter Second Last Name: ")
+    // var second string
+    // fmt.Scanln(&second)
 
     // Print function is used to
     // display output in the same line
-    fmt.Print("Your Full Name is: ")
+    // fmt.Print("Your Full Name is: ")
 
     // Addition of two string
-    fmt.Print(first + " " + second)
+    // fmt.Print(first + " " + second)
+
+    name()
 }
